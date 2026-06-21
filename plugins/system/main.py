5209ba55ce66cea7a5cb274392999d9ad81a8182
冷曦 <2218872014@qq.com>
2026-06-21 17:23:32 +0800

    更新 admin.py

--- plugins/system/main.py
+++ plugins/system/main.py
@@ -12,7 +12,7 @@ __plugin_meta__ = {
     'name': '系统管理',
     'author': 'ElainaBot',
     'description': '框架内置系统插件, 提供基础信息、管理、查询、示例等功能',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'github': 'https://github.com/ElainaCore/ElainaBot_v2',
 }
 
